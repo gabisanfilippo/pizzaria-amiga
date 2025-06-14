2bb1b56fa2a0b546a5983f6e10bced50152ef533
Gabrielle Coelho Sanfilippo <gabriellecoelhosanfilippo@gmail.com>
2025-06-14 15:53:03 -0300

    feat: add PIZZAS constant with details for pizza options

---
+++ src/pages/Home/constants/pizzas.ts
@@ -0,0 +1,38 @@
+import { Mozzarella } from "../../../assets/svgs/Mozzarella";
+import { Pepperoni } from "../../../assets/svgs/Pepperoni";
+import { Mushrooms } from "../../../assets/svgs/Mushrooms";
+import { Margarita } from "../../../assets/svgs/Margarta";
+import { Bacon } from "../../../assets/svgs/Bacon";
+
+export const PIZZAS = [
+  {
+    title: "Marguerita",
+    subtitle: "Solicitar ajuda urgente",
+    path: "/helps",
+    image: Margarita,
+  },
+  {
+    title: "Calabresa",
+    subtitle: "Informações sobre direitos",
+    path: "/rights",
+    image: Pepperoni,
+  },
+  {
+    title: "Mussarela",
+    subtitle: "Contatos de apoio e abrigos",
+    path: "/helps",
+    image: Mozzarella,
+  },
+  {
+    title: "Bacon",
+    subtitle: "Dicas e orientações psicológicas",
+    path: "/guidelines",
+    image: Bacon,
+  },
+  {
+    title: "Monte sua pizza",
+    subtitle: "Personalizar um pedido de ajuda",
+    path: "/customize",
+    image: Mushrooms,
+  },
+];
