15e98ed31ce5697212cc08c74ddef57164006301
Gabrielle Coelho Sanfilippo <gabriellecoelhosanfilippo@gmail.com>
2025-06-16 06:50:23 -0300

    feat: add more information section with special ingredients and delivery addresses

---
+++ src/pages/Home/constants/moreInformation.ts
@@ -0,0 +1,31 @@
+import { GreenPepper } from "../../../assets/svgs/GreenPepper";
+import { Mushroom } from "../../../assets/svgs/Mushroom";
+import { Cheese } from "../../../assets/svgs/Cheese";
+import { Basil } from "../../../assets/svgs/Basil";
+
+export const moreInformation = [
+  {
+    title: "Ingredientes Especiais",
+    subtitle: "Saiba mais sobre medidas protetivas e direitos legais",
+    path: "",
+    image: Basil,
+  },
+  {
+    title: "Endereços de Entrega",
+    subtitle: "Confira endereços de abrigos e locais seguros",
+    path: "",
+    image: Cheese,
+  },
+  {
+    title: "Nossos Fornecedores",
+    subtitle: "Contatos de delegacias da mulher e ONGs",
+    path: "",
+    image: GreenPepper,
+  },
+  {
+    title: "História da nossa Pizzaria",
+    subtitle: "Relatos de sobreviventes e histórias inspiradoras",
+    path: "",
+    image: Mushroom,
+  },
+];
