758aba9776bdec2b5eefcc875d73990b5a5f6bc9
Gabrielle Coelho Sanfilippo <gabriellecoelhosanfilippo@gmail.com>
2025-06-16 07:54:17 -0300

    feat: add HOUSE_FLAVORS constant with pizza GIFs and descriptions

---
+++ src/pages/Home/constants/houseFlavors.ts
@@ -0,0 +1,21 @@
+import pizzaDisassembling from "../../../assets/gifs/pizza-disassembling.gif";
+import pizzaAssembling from "../../../assets/gifs/pizza-assembling.gif";
+
+export const HOUSE_FLAVORS = [
+  {
+    title: "Pique-esconde",
+    subtitle:
+      "Clique aqui e mude o app para algo genérico caso alguém desconfie. Depois é só clicar de novo para voltar.",
+    gif: pizzaDisassembling,
+    path: "",
+    alt: "Gif de uma pizza sendo desmontada por fatias",
+  },
+  {
+    title: "Melhor amiga",
+    subtitle:
+      "Aqui temos contatos de apoio (advogada, psicóloga, assistente social, policial civil, etc).",
+    gif: pizzaAssembling,
+    path: "",
+    alt: "Gif de uma pizza aparecendo aos poucos enquanto gira",
+  },
+];
