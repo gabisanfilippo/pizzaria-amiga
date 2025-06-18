c5bfc4788757056c5791334ec1cb3505be00a9d0
Gabrielle Coelho Sanfilippo <gabriellecoelhosanfilippo@gmail.com>
2025-06-18 08:31:34 -0300

    feat: add INGREDIENTS_LIST constant for ingredient information

---
+++ src/pages/Home/constants/ingredientsList.ts
@@ -0,0 +1,14 @@
+export const INGREDIENTS_LIST = [
+  {
+    title: "Mussarela - 180",
+    subtitle: "Central de Atendimento à Mulher (ligação gratuita, 24h)",
+  },
+  {
+    title: "Tomate - 190",
+    subtitle: "Polícia Militar (em casos de emergência imediata)",
+  },
+  {
+    title: "Manjericão - 100",
+    subtitle: "Direitos Humanos (casos de violência contra mulheres e outros)",
+  },
+];
