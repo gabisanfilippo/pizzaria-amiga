1e553a655d0a6429809f6e88e6f40de727f56e40
Gabrielle Coelho Sanfilippo <gabriellecoelhosanfilippo@gmail.com>
2025-06-18 08:31:14 -0300

    feat: add ADDRESSES_PICK_UP constant for support service locations

---
+++ src/pages/Home/constants/addressesPickUp.ts
@@ -0,0 +1,20 @@
+export const ADDRESSES_PICK_UP = [
+  {
+    title: "Unidade 1",
+    subtitle:
+      "ONG Mães da Sé (local para desaparecidos e apoio a vítimas de violência)",
+  },
+  {
+    title: "Unidade 2",
+    subtitle: "Casa da Mulher Brasileira (em várias cidades do Brasil)",
+  },
+  {
+    title: "Unidade 3",
+    subtitle: "Instituto Maria da Penha (para orientação e suporte jurídico)",
+  },
+  {
+    title: "Unidade 4",
+    subtitle:
+      "APOENA (Atendimento psicológico e acolhimento em situações de violência)",
+  },
+];
