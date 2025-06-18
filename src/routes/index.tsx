172ad65899dcbb1cad389bb6d54c210aa97fb0b9
Gabrielle Coelho Sanfilippo <gabriellecoelhosanfilippo@gmail.com>
2025-06-18 07:24:39 -0300

    feat: add HOME constant for route path in AppRoutes component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
+
+import { HOME } from "../constants/paths";
 import { Home } from "../pages/Home";
 
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path={HOME} element={<Home />} />
     </Routes>
   );
 };
