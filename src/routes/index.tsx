54c3751ba8e748910d73a1825003b1a5b5247397
Gabrielle Coelho Sanfilippo <gabriellecoelhosanfilippo@gmail.com>
2025-06-25 09:58:35 -0300

    feat: add route for InformationRights page in AppRoutes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 
-import { HOME } from "../constants/paths";
+import { InformationRights } from "../pages/InformationRights";
+import { HOME, INFORMATION_RIGHTS } from "../constants/paths";
 import { Home } from "../pages/Home";
 
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route path={HOME} element={<Home />} />
+      <Route path={INFORMATION_RIGHTS} element={<InformationRights />} />
     </Routes>
   );
 };
