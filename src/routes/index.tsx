b4cdb65ddd4b048d36fedc691ccb5f00987a2450
Gabrielle Coelho Sanfilippo <gabriellecoelhosanfilippo@gmail.com>
2025-06-25 10:54:17 -0300

    feat: reorder routes in AppRoutes for improved structure

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,20 @@
 import { Routes, Route } from "react-router-dom";
 
 import { InformationRights } from "../pages/InformationRights";
-import { HOME, INFORMATION_RIGHTS } from "../constants/paths";
+import { ComfortingCombos } from "../pages/ComfortingCombos";
 import { Home } from "../pages/Home";
+import {
+  INFORMATION_RIGHTS,
+  COMFORTING_COMBOS,
+  HOME,
+} from "../constants/paths";
 
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path={HOME} element={<Home />} />
       <Route path={INFORMATION_RIGHTS} element={<InformationRights />} />
+      <Route path={COMFORTING_COMBOS} element={<ComfortingCombos />} />
+      <Route path={HOME} element={<Home />} />
     </Routes>
   );
 };
